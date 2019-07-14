0246dc8038959ce7c35d04fef65665723987609a
Pavel Druzhinin <pavel.druzhinin@gmail.com>
2019-07-14 14:20:35 +0300

    first commit

---
+++ posts/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def posts_home(request):
+    return HttpResponse('<h1>Hello Posts!</h1>')
