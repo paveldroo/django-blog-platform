a9f9ec76f383cc0fa38f8724c8cf4bd0a6aee9c9
Pavel Druzhinin <pavel.druzhinin@gmail.com>
2019-07-15 11:43:45 +0300

    Added URLs and template

---
+++ posts/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from .views import post_create, post_delete, post_detail, post_list, post_update
+
+urlpatterns = [
+    url(r'^$', post_list),
+    url(r'^create/$', post_create),
+    url(r'^detail/$', post_detail),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
+]
--- posts/views.py
+++ posts/views.py
@@ -4,5 +4,24 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def posts_home(request):
-    return HttpResponse('<h1>Hello Posts!</h1>')
+def post_list(request):
+    context = {
+        'title': 'Post List'
+    }
+    return render(request, 'index.html', context)
+
+
+def post_create(request):
+    return HttpResponse('<h1>Create</h1>')
+
+
+def post_detail(request):
+    return HttpResponse('<h1>Detail</h1>')
+
+
+def post_update(request):
+    return HttpResponse('<h1>Update</h1>')
+
+
+def post_delete(request):
+    return HttpResponse('<h1>Delete</h1>')
