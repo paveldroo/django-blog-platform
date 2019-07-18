0354b1935e4d9ba12c98da7f5d30d1b20ca965d8
Pavel Druzhinin <pavel.druzhinin@gmail.com>
2019-07-18 11:05:10 +0300

    Added deleting post + django messages + redirects

--- posts/urls.py
+++ posts/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from .views import post_create, post_delete, post_detail, post_list, post_update
 
 urlpatterns = [
-    url(r'^$', post_list),
+    url(r'^$', post_list, name='list'),
     url(r'^create/$', post_create),
-    url(r'^(?P<id>\d+)', post_detail, name='detail'),
-    url(r'^update/$', post_update),
-    url(r'^delete/$', post_delete),
+    url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
+    url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
+    url(r'^(?P<id>\d+)/delete/$', post_delete),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from posts.forms import PostForm
@@ -20,12 +21,16 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, 'Post successfully created!')
+        return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, 'Post not created!')  # Fail warning always shows and twice
 
     context = {
         'form': form
     }
 
-    return render(request, 'post_create.html', context)
+    return render(request, 'post_form.html', context)
 
 
 def post_detail(request, id=None):
@@ -37,9 +42,26 @@ def post_detail(request, id=None):
     return render(request, 'post_detail.html', context)
 
 
-def post_update(request):
-    return HttpResponse('<h1>Update</h1>')
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "<a href='#'>Post</a> successfully Updated!", extra_tags='html_safe')
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        'title': instance.title,
+        'instance': instance,
+        'form': form
+    }
+
+    return render(request, 'post_form.html', context)
 
 
-def post_delete(request):
-    return HttpResponse('<h1>Delete</h1>')
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, 'Post Successfully Deleted!')
+    return redirect('posts:list')
